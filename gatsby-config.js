a64c22ff78aca8d0ee6b15ad328ca57b5f3fbbf1
Muhu-Muza <schumannivan@gmail.com>
2023-07-15 16:28:40 +0300

    added files

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [],
+  siteMetadata: {
+    title: 'Web Warrior',
+    description: 'web dev portfolio',
+    copyright: 'Copyright @Muhumuza Ivan 2023'
+  },
+}
