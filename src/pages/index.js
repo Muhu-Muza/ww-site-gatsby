1b0d48b6628288e614e49bc557e4909a5c367731
Muhu-Muza <schumannivan@gmail.com>
2023-07-16 13:32:32 +0300

    migrated from the old gatsby-image to gatsby-plugin-image and made the necesasry adjustments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,47 @@
 import React from "react"
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from "../components/Layout"
 import * as styles from '../styles/home.module.css'
+// import Img from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout>
       <section className={styles.header}>
         <div>
-          <h2>Design</h2>
-          <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer based in Manchester.</p>
+          {/* <h3>Design</h3> */}
+          <h2>Design, Develop & Deploy</h2>
+          <h3>Full Stack developer</h3>
+          <p>Ready to bring my skills to your service.</p>
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }} />
+        {/* <Img fluid={data.file.childImageSharp.fluid} /> */}
+        <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} />
       </section>
     </Layout>
   )
 }
+
+// export const query = graphql`
+// query Banner {
+//   file(relativePath: {eq: "banner.png"}) {
+//     childImageSharp {
+//       id
+//       fluid {
+//         ...GatsbyImageSharpFluid
+//       }
+//     }
+//   }
+// }`
+export const query = graphql`
+query Banner {
+  file(relativePath: {eq: "banner.png"}) {
+    childImageSharp {
+        gatsbyImageData(
+          width: 768
+          formats: [AUTO, WEBP, AVIF]    
+          )
+    }
+  }
+}`
